dd3ec46f0113b02d5dd0fbbb1a193371dda66828
Monika Schrenk <moni@random-access.org>
2015-11-28 03:30:10 +0100

    Completed Javadoc for package ess.utils in Data_Component

--- Data_Component/src/ess/utils/PropertyException.java
+++ Data_Component/src/ess/utils/PropertyException.java
@@ -1,24 +1,35 @@
 package ess.utils;
-// TODO Javadoc
 
+/**
+ * This exception is thrown when a problem occurs in the context of the property
+ * file, this means normally a typo in a value or a key, or a missing key or
+ * value.
+ * 
+ * @author Monika Schrenk
+ *
+ */
 public class PropertyException extends Exception {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public PropertyException(String message) {
-		super(message);
-	}
+    /**
+     * Constructs a new PropertyException with the given error message.
+     * 
+     * @param message an error message.
+     */
+    public PropertyException(String message) {
+        super(message);
+    }
 
-	public PropertyException(Throwable cause) {
-		super(cause);
-	}
-
-	public PropertyException(String message, Throwable cause) {
-		super(message, cause);
-	}
-
-	public PropertyException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-	}
+    /**
+     * Constructs a new PropertyException with the given error message
+     * and the cause of this exception as a Throwable
+     * 
+     * @param message an error message.
+     * @param cause a Throwable causing this exception to be thrown
+     */
+    public PropertyException(String message, Throwable cause) {
+        super(message, cause);
+    }
 
 }
